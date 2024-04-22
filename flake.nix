c9b96e164e749473856fa354481fb1645068ab62
Marc M <07marczoyd.murray@gmail.com>
2024-04-22 20:58:44 +0200

    hyprland

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
@@ -24,6 +25,15 @@
           inputs.home-manager.nixosModules.default
         ];
       };
+    homeConfigurations = {
+      default = inputs.home-manager.lib.homeManagerConfiguration {
+        specialArgs = {inherit inputs;};
+        modules = [
+          inputs.hyprland.homeManagerModules.default
+          {wayland.windowManager.hyprland.enable = true;}
+        ];
+      };
+    };
     };
   };
 }
--- modules/home-manager/wm/hyprland.nix
+++ modules/home-manager/wm/hyprland.nix
@@ -3,5 +3,93 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+    settings = {
+      monitor=",1920x1080@74.97,auto,auto";
+      exec-once = "waybar & hyprpaper & gammastep -O 3000 & emacs --daemon";
+      "$terminal" = "foot";
+      "$fileManager" = "dolphin";
+      "$menu" = "wofi --show drun";
+      env = [
+        "XCURSOR_SIZE,24"
+        "QT_QPA_PLATFORMTHEME,qt5t"
+      ];
+      input = {
+          kb_layout = "us";
+          kb_variant = "altgr-intl";
+      };
+      general = {
+          gaps_in = 3;
+          gaps_out = 3;
+          border_size = 2;
+          col.active_border = "rgba(bcc9cfaa)";
+          col.inactive_border = "rgba(2B303Caa)";
+
+          layout = "dwindle";
+      };
+      dwindle = {
+          pseudotile = "yes";
+          preserve_split = "yes"; # you probably want this
+          force_split = 2;
+      };
+      master = {
+          new_is_master = true;
+      };
+      "$mainMod" = "ALT";
+      bind = [
+        "$mainMod SHIFT, I, exec, $terminal"
+        "$mainMod SHIFT, C, killactive,"
+        "$mainMod SHIFT, Q, exit,"
+        "$mainMod, F, exec, firefox"
+        "$mainMod, E, exec, bash em"
+        "$mainMod, Y, exec, spotify-launcher "
+        "$mainMod, D, exec, discord "
+        "$mainMod, V, togglefloating, "
+        "$mainMod, R, exec, $menu"
+        "$mainMod SHIFT, U, togglesplit, # dwindle"
+        "$mainMod SHIFT, X, exec, grim -g \"$(slurp)\" - | wl-copy && wl-paste > ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png"
+        "$mainMod, N, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%"
+        "$mainMod, M, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%"
+        
+        "$mainMod, h, movefocus, l"
+        "$mainMod, l, movefocus, r"
+        "$mainMod, k, movefocus, u"
+        "$mainMod, j, movefocus, d"
+        
+        "$mainMod SHIFT, h, movewindow, l"
+        "$mainMod SHIFT, l, movewindow, r"
+        "$mainMod SHIFT, k, movewindow, u"
+        "$mainMod SHIFT, j, movewindow, d"
+        
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+        
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+      ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
   };
 }
