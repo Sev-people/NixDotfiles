733e6790d55a17038c98e4d276251a1804baec18
Marc M <07marczoyd.murray@gmail.com>
2024-04-23 15:04:31 +0200

    impure emacs config

---
+++ modules/home-manager/editors/emacs/emacs.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+
+  xdg.configFile.".config/emacs/init.el".source = ./init.el;
+  xdg.configFile.".config/emacs/early-init.el".source = ./early-init.el;
+  xdg.configFile.".config/emacs/config.org".source = ./config.org;
+
+}
