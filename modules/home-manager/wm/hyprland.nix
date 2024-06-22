3c028cfc4d7663426dc300cb5a616e5cfebd6982
Marc M <07marczoyd.murray@gmail.com>
2024-06-22 13:07:05 +0200

    Changes

--- modules/home-manager/wm/hyprland.nix
+++ modules/home-manager/wm/hyprland.nix
@@ -5,7 +5,7 @@
     xwayland.enable = true;
     settings = {
       monitor=",1920x1080@74.97,auto,auto";
-      exec-once = ["waybar" "swww-daemon --format xrgb" "gammastep -O 3000" "emacs --daemon --init-directory ~/.config/emacs"];
+      exec-once = ["waybar" "swww-daemon --format xrgb" "gammastep -O 3000" "emacs --daemon --init-directory ~/.dotfiles/emacs"];
       "$terminal" = "foot";
       "$fileManager" = "ranger";
       "$menu" = "wofi --show drun";
