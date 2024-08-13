1a1d11109723cf08981dcb22005d24009eda829f
Sev-people <07marczoyd.murray@gmail.com>
2024-08-13 10:42:12 +0200

    Changes

--- flake.nix
+++ flake.nix
@@ -24,6 +24,13 @@
           inputs.home-manager.nixosModules.default
         ];
       };
+      laptop = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/laptop/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
 }
