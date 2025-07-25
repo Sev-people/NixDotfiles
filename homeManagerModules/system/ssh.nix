f0aace2f76b7c8a1617d2f58015c012963fed87a
Sev-people <07marczoyd.murray@gmail.com>
2025-07-25 21:50:00 +0200

    Changes

--- homeManagerModules/system/ssh.nix
+++ homeManagerModules/system/ssh.nix
@@ -1,21 +1,13 @@
 { ... }:
 
-let
-  localVars = import ../../local-variables.nix;
-in {
+{
 
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      "myserver" = {
+      desktop = {
         hostname = "default";
         user = "marc";
-        identityFile = "~/.ssh/id_ed25519";
-      };
-      termux = {
-        hostname = localVars.termuxIp;
-        port = 8022;
-        user = "u0_a464";
         identityFile = "~/.ssh/id_ed25519";
       };
     };
