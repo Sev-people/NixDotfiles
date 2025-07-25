b4d3b625e5771936e09743be9d5601e988616227
Sev-people <07marczoyd.murray@gmail.com>
2025-07-25 23:20:40 +0200

    113 current  2025-07-25 23:20:31  24.11.20250630.50ab793  6.6.94                          *

--- homeManagerModules/scripts/scripts.nix
+++ homeManagerModules/scripts/scripts.nix
@@ -20,7 +20,7 @@ let
   track-add = pkgs.writeShellScriptBin "track-add" ''
     set -euo pipefail
     
-    if [ -z "$\{1-}" ]; then
+    if [ -z "$${1-}" ]; then
       echo "Usage: $0 <YouTube-URL>"
       exit 1
     fi
@@ -60,14 +60,14 @@ let
     # Start an infinite loop
     while true; do
         # Check if the wallpapers array is empty
-        if [ $\{#wallpapers[@]} -eq 0 ]; then
+        if [ $${#wallpapers[@]} -eq 0 ]; then
             # If the array is empty, refill it with the image files
             wallpapers=("$wallpapersDir"/*)
         fi
     
         # Select a random wallpaper from the array
-        wallpaperIndex=$(( RANDOM % $\{#wallpapers[@]} ))
-        selectedWallpaper="$\{wallpapers[$wallpaperIndex]}"
+        wallpaperIndex=$(( RANDOM % $${#wallpapers[@]} ))
+        selectedWallpaper="$${wallpapers[$wallpaperIndex]}"
     
         # Update the wallpaper using the swww img command
         swww img "$selectedWallpaper" -t right --transition-fps 60
