0a7634f8e4ab4713ebac95263188a3353e9731ff
Sev-people <07marczoyd.murray@gmail.com>
2025-08-12 15:08:07 +0200

    Auto-commit on 2025-08-12 15:08:07 [master]

--- homeManagerModules/system/notifications.nix
+++ homeManagerModules/system/notifications.nix
@@ -8,18 +8,12 @@ in {
     enable = true;
     settings = {
       global = {
-        width = 400;
-        height = 100;
-        offset = "30x50";
         origin = "top-right";
         transparency = 10;
-        frame_color = style.colors.base01;
         font = "${style.font.main} ${toString (style.font.size)}";
       };
     
       urgency_normal = {
-        background = style.colors.base00;
-        foreground = style.colors.regular0;
         timeout = 10;
       };
     };
